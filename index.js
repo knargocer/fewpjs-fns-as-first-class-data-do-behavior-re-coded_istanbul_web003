d268d365e86ed25cc72d354a97d91be34b491461
Knar Gocer <knargr@gmail.com>
2020-07-23 12:13:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+
+function greet(str){
+  const hour = str.split(":")[0].parseInt;
+  const mint = str.split(":")[1].parseInt;
+  
+  if(hour<12){
+    return "Good Morning";
+  }
+  else if(hour>12 && hour>17){
+    return "Good Afternoon";
+  }
+  else if(hour>17){
+    return  "Good Evening";
+  }
+}
+
+function displayMessage(str){
+  document.getElementById("greeting").innerHTML = "str" ;
+}
+
