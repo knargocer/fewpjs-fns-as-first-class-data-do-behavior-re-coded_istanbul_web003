1becf94d42b80543a8e21040a23f4dd57ac4eab4
Knar Gocer <knargr@gmail.com>
2020-07-23 12:18:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function greet(str){
   if(hour<12){
     return "Good Morning";
   }
-  else if(hour>12 && hour>17){
+  else if(hour>12 && hour<17){
     return "Good Afternoon";
   }
   else if(hour>17){
